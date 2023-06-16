7b31a776d3cf6f5f4bb6c12f23afb58964ed8769
TohyaZ <heavencodd@gmail.com>
2023-06-16 15:26:22 +0700

    Buoi-1

---
+++ buoi-1/bt1.js
@@ -0,0 +1,6 @@
+function reverseString(str) {
+    return str.split("").reverse().join("")
+}
+console.log(reverseString("hello")) 
+
+// split chuyển 1 chuỗi sang ký tự - reverse đảo ngược mảng các ký tự - join gộp các ký tự lại thành một mảng

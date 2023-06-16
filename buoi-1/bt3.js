e13ff8a4f24c91ab578ca0c22508cf3ca703ff88
TohyaZ <heavencodd@gmail.com>
2023-06-16 15:26:43 +0700

    Buoi-1

--- buoi-1/bt3.js
+++ buoi-1/bt3.js
@@ -2,19 +2,19 @@ function mostFrequent(arr) {
     let map = new Map();
     let maxCount = 0;
     let maxItem = null;
-  
+
     for (let item of arr) {
-      let count = map.has(item) ? map.get(item) + 1 : 1;
-      map.set(item, count);
-      if (count > maxCount) {
+    let count = map.has(item) ? map.get(item) + 1 : 1;
+    map.set(item, count);
+    if (count > maxCount) {
         maxCount = count;
         maxItem = item;
-      }
     }
-  
+    }
+
     return maxItem;
-  }
-  
-  let arr = [1, 2, 4, 4, 3, 4, 5, 5, 6];
-  let mostFrequentItem = mostFrequent(arr);
-  console.log(mostFrequentItem);
+}
+
+let arr = [1, 2, 4, 4, 3, 4, 5, 5, 6];
+let mostFrequentItem = mostFrequent(arr);
+console.log(mostFrequentItem);

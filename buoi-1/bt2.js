e6bc22f3c0500df3941604b0522c347baf4d3bf8
NamTran <92435056+TohyaZ@users.noreply.github.com>
2023-06-23 17:46:42 +0700

    Delete buoi-1 directory

--- buoi-1/bt2.js
+++
@@ -1,7 +0,0 @@
-function unique(arr) {
-    return Array.from(new Set(arr)) //
-  }
-  console.log(unique([1, 2, 3, 4, 5, 6, 7, 8, 4, 4, 4, 5]))
-
-// Hàm Set(arr) dc dùng để tạo ra một hàm mới mà các phần tử trong này không bị trùng lập
-// Hàm Array.from dc dùng để chuyển đổi thành một mảng chứa các phần tử duy nhất của Set(arr)
